28f4eaa9bf007fa2576a8c6c1a954c362282b791
Mahima <nagpalmahima63@gmail.com>
2025-08-05 15:12:08 +0530

    final code
    ecommerce mern stack app

---
+++ routes/productRoutes.js
@@ -0,0 +1,60 @@
+import express from "express";
+import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
+import { createProductController, deleteProductController, getProductController,
+     getSingleProductController, productCountController, productFiltersController, 
+     productListController, productPhotoController, searchProductController,
+      updateProductController, relatedProductController,
+       productCategoryController,
+       braintreeTokenController,
+       brainTreePaymentController,
+     } from "../controllers/productController.js";
+import formidable from 'express-formidable';
+
+const router = express.Router();
+
+// routes
+router.post("/create-product", requireSignIn, isAdmin, formidable(), createProductController);
+
+// get products
+router.get('/get-product', getProductController);
+
+// single product
+router.get('/get-product/:slug', getSingleProductController);
+
+// get photo
+router.get('/product-photo/:pid', productPhotoController);
+
+// delete product
+router.delete('/delete-product/:pid', deleteProductController);
+
+//filter product
+router.post("/product-filters", productFiltersController);
+
+// update product
+router.put("/update-product/:pid", requireSignIn, isAdmin, formidable(), updateProductController);
+
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
+//search product
+router.get("/search/:keyword", searchProductController);
+
+// similar product
+router.get('/related-product/:pid/:cid', relatedProductController);
+
+// category wise product
+router.get('/product-category/:slug', productCategoryController);
+
+//payments routes
+//token
+router.get("/braintree/token", braintreeTokenController);
+
+//payments
+router.post("/braintree/payment", requireSignIn, brainTreePaymentController);
+
+
+
+export default router;
---
+++ server.js
@@ -0,0 +1,51 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from "morgan";
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+import cors from 'cors';
+import productRoutes from './routes/productRoutes.js';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+// configure env
+dotenv.config();
+
+// database config
+connectDB();
+
+// esnodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// rest object
+const app= express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// routes
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/category', categoryRoutes);
+app.use('/api/v1/product', productRoutes);
+
+// static files
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('*', function(req,res){
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+// rest api
+app.get('/',(req,res)=>{
+    res.send("<h1>welcome to ecommerce app</h1>")
+});
+
+// port
+const port= process.env.port || 8080;
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`);
+});
